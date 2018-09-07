c863e27667491f351074fe5e9e8a54bd5a08abf3
ADITYA PRIYADARSHI <adityapriyadarshiex@gmail.com>
2018-09-07 20:50:35 +0530

    Add files via upload

---
+++ chat/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAC6yFIX6pEBAJHIPwI4expTT1p1jqjbmscand2mQ904H4qo2jEsl5qGpAMrqdVd9UO4gzU0FAzhXD09C9bb3JQv6sRxaeUrV9A5ghuThSexcTLQcqGaqAMhqJZAMbvnNHS4HlIb0lI9YZBsADzt2j2pL6iVPx9VunHnXPLAZDZD',
+    FB_VERIFY_TOKEN: 'aditz23',
+    API_AI_CLIENT_ACCESS_TOKEN: '33fe0d83303545f18ead6ef30fcdee90',
+    FB_APP_SECRET: '89089c85f6cf049cd7958ef07e1f9b6a',
+    SERVER_URL: "https://aims2.herokuapp.com/",
+};
